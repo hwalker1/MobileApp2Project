53769ca60e56cddbe4d1f60b342115f32ff4d582
Henry <henrywalker@gmail.com>
2019-03-14 23:26:33 -0400

    Added Joystick and buttons
    also cleaned up a few things asset wise

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Button : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public bool pressed;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float playerSpeed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float grav = 20.0f;
+    private Joystick joystick;
+    private Button leftButton;
+    private Button rightButton;
+    private Button jumpButton;
+    private Vector3 moveDirection; 
+    private CharacterController playerController;
+
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+        joystick = FindObjectOfType<Joystick>();
+        jumpButton = FindObjectsOfType<Button>()[0];
+        rightButton = FindObjectsOfType<Button>()[1];
+        leftButton = FindObjectsOfType<Button>()[2]; 
+    }
+
+    void Update()
+    {
+        if (playerController.isGrounded)
+        {     
+            moveDirection = new Vector3(joystick.Horizontal, 0.0f, joystick.Vertical); //get input from joysticks
+            moveDirection = transform.TransformDirection(moveDirection); //compare it to where character is facing 
+            moveDirection = moveDirection * playerSpeed; 
+
+            if (jumpButton.pressed)
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+        moveDirection.y = moveDirection.y - (grav * Time.deltaTime);
+        playerController.Move(moveDirection * Time.deltaTime);
+          
+        //Turn with O and P
+        if (rightButton.pressed)
+        {
+            playerController.transform.Rotate(0, -2, 0);
+        }
+        if (leftButton.pressed)
+        {
+            playerController.transform.Rotate(0, 2, 0);
+        }
+       
+    }
+}
